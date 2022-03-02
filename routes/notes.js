33b0ced3839e419ee71bfe721291642c10470d7d
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-03-02 11:51:47 -0800

    Created routes for home page and notes page

---
+++ routes/notes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+// Get Route for retrieving all of the saved notes
+router.get("/", (req, res) => {
+
+});
+
+// Post Route for a new note
+router.post("/", (req, res) => {
+
+});
+
+// Delete route for a specific note
+router.delete("/:id", (req, res) => {
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", api);
+
+// Get Route for home page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Get Route for notes page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// Wild card route to direct users to home page
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, () => 
+    console.log(`App listening at 'https://localhost:${PORT}`)
+);
